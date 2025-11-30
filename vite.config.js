4f42caf7e9742c12c70eb7d9f9807aa45ca244b1
RaadShahamat <raadalif25@gmail.com>
2025-11-30 10:58:50 +0600

    fixed the vite package issue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Raad_Portfolio/", // <-- Replace with your GitHub repo name
+  base: "/Raad-Portfolio/", // <-- Replace with your GitHub repo name
   plugins: [react()],
 });
